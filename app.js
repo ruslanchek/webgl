5046fcb3facc3ed9f25f9eb3d14c0654669b4454
Ruslan Shashkov <ruslan.shashkov@ibitcy.com>
2015-09-16 19:30:56 +0300

    SVG

--- app.js
+++ app.js
@@ -1,49 +1,142 @@
-function webglAvailable() {
-	try {
-		var canvas = document.createElement('canvas');
-		return !!(window.WebGLRenderingContext && (
-			canvas.getContext('webgl') ||
-			canvas.getContext('experimental-webgl')));
-	} catch(e) {
-		return false;
+var ChartArea = function(scene) {
+	var _this = this,
+		gradient = null,
+		areaPolygon = null,
+		line = null,
+		animationDuration = 300,
+		sceneHeight = 0,
+		animationEnabled = false,
+		sceneWidth = 0;
+
+	function init() {
+		if(scene) {
+			updateSceneDimensions();
+			gradient = createAreaGradient();
+			areaPolygon = createAreaPolygon();
+			line = createLine();
+		}
 	}
-}
 
-if(webglAvailable()) {
-	renderer = new THREE.WebGLRenderer({ antialiasing: true });
-} else {
-	renderer = new THREE.CanvasRenderer();
-}
+	function updateSceneDimensions() {
+		sceneWidth = parseFloat(scene.node.clientWidth);
+		sceneHeight = parseFloat(scene.node.clientHeight);
+	}
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+	function createAreaGradient() {
+		var gradient = scene.gradient('linear', function(stop) {
+			stop.at({
+				offset: 0,
+				color: '#1594D1',
+				opacity: 0.3
+			});
 
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
-camera.lookAt(new THREE.Vector3(0, 0, 0));
-var scene = new THREE.Scene();
+			stop.at({
+				offset: 1,
+				color: '#1594D1',
+				opacity: 0.075
+			});
+		});
 
-var material = new THREE.LineBasicMaterial({
-	color: 0x0000ff
-});
+		gradient.from(0, 0).to(0, 1);
 
-var geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+		return gradient;
+	}
 
-var line = new THREE.Line(geometry, material);
+	function getAreaConnectedPoints(points) {
+		return _.union([[points[points.length - 1][0], sceneHeight]], [[points[0][0], sceneHeight]], points);
+	}
 
-scene.add(line);
+	function createAreaPolygon() {
+		return scene.polygon().fill({
+			color: gradient
+		});
+	}
 
+	function createLine() {
+		return scene.polyline().fill('none').stroke({
+			width: 2,
+			color: '#1595D3'
+		});
+	}
 
-var render = function () {
-	requestAnimationFrame( render );
-	renderer.render(scene, camera);
+	function drawLine(points) {
+		line
+			.animate(getAnimationDuration())
+			.plot(points);
+	}
+
+	function drawAreaPolygon(points) {
+		areaPolygon
+			.animate(getAnimationDuration())
+			.plot(getAreaConnectedPoints(points));
+	}
+
+	function getAnimationDuration() {
+		return(animationEnabled) ? animationDuration : 0;
+	}
+
+	function convertPoints(points) {
+		var newPoints = [];
+
+		_.each(points, function(point) {
+			newPoints.push([
+				point[0],
+				sceneHeight - point[1]
+			]);
+		});
+
+		return newPoints;
+	}
+
+	function draw(points, animate) {
+		points = convertPoints(points);
+
+		animationEnabled = animate;
+
+		if(scene && points && points.length > 0) {
+			updateSceneDimensions();
+			drawLine(points);
+			drawAreaPolygon(points);
+		}
+	}
+
+	_this.draw = draw;
+
+	init();
 };
 
-render();
+var scene = SVG('scene').size($(window).width(), $(window).height() / 2);
+var scene1 = SVG('scene1').size($(window).width(), $(window).height() /2);
+var chartArea = new ChartArea(scene);
+var chartArea1 = new ChartArea(scene1);
 
-window.onresize = function(){
-renderer.setSize(window.innerWidth, window.innerHeight);
-}
+var points = [
+	[2, 100],
+	[5, 104]
+];
+
+_.times(100, function(){
+    points.push([
+    	points[points.length - 1][0] + 5,
+    	_.random(_.random(400, 450), _.random(450, 500))
+    ]);
+});
+
+chartArea.draw(points, false);
+chartArea1.draw(points, false);
+
+setInterval(function() {
+	points.push([
+		points[points.length - 1][0] + 5,
+		_.random(_.random(400, 450), _.random(450, 500))
+	]);
+
+	chartArea.draw(points, true);
+	chartArea1.draw(points, true);
+}, 1000);
+
+$(window).on('resize', function() {
+	scene.size($(window).width(), $(window).height());
+	chartArea.draw(points, true);
+	chartArea1.draw(points, true);
+});
