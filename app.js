ff29619dd5d2070111ee657db337a7b01d411c01
Ruslan Shashkov <ruslan.shashkov@ibitcy.com>
2015-09-17 10:08:27 +0300

    Scene

--- app.js
+++ app.js
@@ -1,49 +1,99 @@
-function webglAvailable() {
-	try {
-		var canvas = document.createElement('canvas');
-		return !!(window.WebGLRenderingContext && (
-			canvas.getContext('webgl') ||
-			canvas.getContext('experimental-webgl')));
-	} catch(e) {
-		return false;
+var ChartLineMaterial = function(){
+
+};
+
+var ChartScene = function(containerId){
+    var _this = this,
+        renderer = null,
+        $container = null,
+        camera = null,
+        renderType = undefined,
+        scene = null;
+
+    function init(){
+        createRenderer();
+        initContainer();
+        resizeScene();
+        createCamera();
+        createScene();
+        render();
+    }
+
+    function initContainer(){
+        $container = $('#' + containerId);
+        $container.append(renderer.domElement);
+    }
+
+	function isWebGLAvailable() {
+		try {
+			var canvas = document.createElement('canvas');
+
+			return !!(window.WebGLRenderingContext && (
+				canvas.getContext('webgl') ||
+				canvas.getContext('experimental-webgl')));
+		} catch(e) {
+			return false;
+		}
 	}
-}
 
-if(webglAvailable()) {
-	renderer = new THREE.WebGLRenderer({ antialiasing: true });
-} else {
-	renderer = new THREE.CanvasRenderer();
-}
+    function createRenderer() {
+        if (isWebGLAvailable()) {
+            renderType = 'WebGL';
+            renderer = new THREE.WebGLRenderer({
+                antialiasing: true
+            });
+        } else {
+            renderType = 'Canvas';
+            renderer = new THREE.CanvasRenderer();
+        }
 
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+        console.log('RENDERER_CONTEXT:', renderType);
+    }
 
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
-camera.position.set(0, 0, 100);
-camera.lookAt(new THREE.Vector3(0, 0, 0));
-var scene = new THREE.Scene();
+    function resizeScene(){
+        renderer.setSize($container.width(), $container.height());
+    }
+
+    function createCamera(){
+        camera = new THREE.PerspectiveCamera(45, $container.width() / $container.height(), 1, 1000);
+        camera.position.set(0, 0, 750);
+        camera.lookAt(new THREE.Vector3(0, 0, 0));
+    }
+
+    function createScene(){
+        scene = new THREE.Scene();
+    }
+
+    function render(){
+        requestAnimationFrame( render );
+        renderer.render(scene, camera);
+    }
+
+    function addToScene(object){
+        scene.add(object);
+    }
+
+    _this.addToScene = addToScene;
+
+    init();
+};
+
+
+var chartScene = new ChartScene('scene');
 
 var material = new THREE.LineBasicMaterial({
-	color: 0x0000ff
+    color: 0x1595D3,
+    linewidth: 2,
+    linecap: 'round',
+    linejoin: 'round'
 });
 
 var geometry = new THREE.Geometry();
+
 geometry.vertices.push(new THREE.Vector3(0, 0, 0));
-geometry.vertices.push(new THREE.Vector3(0, 10, 0));
-geometry.vertices.push(new THREE.Vector3(10, 0, 0));
+geometry.vertices.push(new THREE.Vector3(0, 20, 0));
+geometry.vertices.push(new THREE.Vector3(20, 0, 0));
 
 var line = new THREE.Line(geometry, material);
 
-scene.add(line);
-
-
-var render = function () {
-	requestAnimationFrame( render );
-	renderer.render(scene, camera);
-};
-
-render();
-
-window.onresize = function(){
-renderer.setSize(window.innerWidth, window.innerHeight);
-}
+chartScene.addToScene(line);
